bc6e56edd56d06315d985d6f3cbdb8a9e84be417
aconkli9 <aconkli9@ford.com>
2022-05-02 13:40:16 -0400

    Add sentence

--- utilities/difference.js
+++ utilities/difference.js
@@ -2,6 +2,7 @@ import { difference } from "lodash"
 
 export function arrayDiffMessage(arrayOne, arrayTwo) {
     const diff = difference(arrayOne, arrayTwo)
+    const reverseDiff = difference(arrayTwo, arrayOne)
 
-    return `The difference between [${arrayOne}] and [${arrayTwo}] is [${diff}].`
+    return `The difference between [${arrayOne}] and [${arrayTwo}] is [${diff}] and the difference between [${arrayTwo}] and [${arrayOne}] is [${reverseDiff}].`
 }
